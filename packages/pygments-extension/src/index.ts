adb81bc53a0d5fdde1ab9b3e1fd7d20801b8a281
martinRenou <martin.renou@gmail.com>
2022-03-04 15:44:11 +0100

    Iterate

--- packages/pygments-extension/src/index.ts
+++ packages/pygments-extension/src/index.ts
@@ -1,3 +1,10 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+/**
+ * @packageDocumentation
+ * @module pygments-extension
+ */
+
 import {
     JupyterFrontEnd,
     JupyterFrontEndPlugin
