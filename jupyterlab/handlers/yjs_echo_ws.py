fade3449de48bad9d19ba65816b6a78563aef895
David Brochart <david.brochart@gmail.com>
2022-03-22 10:47:43 +0100

    Support awareness message filtering

--- jupyterlab/handlers/yjs_echo_ws.py
+++ jupyterlab/handlers/yjs_echo_ws.py
@@ -3,14 +3,17 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import json
 import uuid
 import time
+from typing import Any, Dict
 
 from tornado.ioloop import IOLoop
 from jupyter_server.base.handlers import JupyterHandler
 from tornado import web
 from tornado.websocket import WebSocketHandler
 from enum import IntEnum
+import y_py as Y
 
 ## The y-protocol defines messages types that just need to be propagated to all other peers.
 ## Here, we define some additional messageTypes that the server can interpret.
@@ -55,6 +58,7 @@ class YjsEchoWebSocket(WebSocketHandler, JupyterHandler):
     def open(self, guid):
         #print("[YJSEchoWS]: open", guid)
         cls = self.__class__
+        self.awareness = Awareness(Y.YDoc(uuid.uuid4().int & (1 << 64) - 1))
         self.id = str(uuid.uuid4())
         self.room_id = guid
         room = cls.rooms.get(self.room_id)
@@ -107,9 +111,17 @@ class YjsEchoWebSocket(WebSocketHandler, JupyterHandler):
             cls.rooms[new_room_id] = room
             # print("renamed room to " + new_room_id + ". Old room name was " + room_id)
         elif room:
-            for client_id, (loop, hook_send_message, client) in room.clients.items() :
-                if self.id != client_id :
-                    loop.add_callback(hook_send_message, message)
+            skip = False
+            if room_id == "JupyterLab:globalAwareness":
+                if message[0] == 1:
+                    changes = get_awareness_changes(self.awareness, message[1:])
+                    # example of awareness message filtering:
+                    # if changes["removed"]:
+                    #     skip = True
+            if not skip:
+                for client_id, (loop, hook_send_message, client) in room.clients.items() :
+                    if self.id != client_id :
+                        loop.add_callback(hook_send_message, message)
 
     def on_close(self):
         # print("[YJSEchoWS]: close")
@@ -128,3 +140,79 @@ class YjsEchoWebSocket(WebSocketHandler, JupyterHandler):
 
     def hook_send_message(self, msg):
         self.write_message(msg, binary=True)
+
+
+class Decoder:
+    def __init__(self, update: bytes):
+        self.update = update
+        self.i = 0
+        self.length = self.read_var_uint()
+
+    def read_var_uint(self):
+        res = 0
+        i = 0
+        while True:
+            v = self.update[self.i]
+            res += (127 & v) << i
+            i += 7
+            self.i += 1
+            if v < 128:
+                break
+        return res
+
+    def read_var_string(self):
+        length = self.read_var_uint()
+        if length == 0:
+            return ""
+        return self.update[self.i:].decode("utf-8")
+
+
+class Awareness:
+    def __init__(self, doc):
+        self.doc = doc
+        self.meta = {}
+        self.states = {}
+        self.client_id = doc.id
+
+    def get_local_state(self):
+        return self.states.get(self.cliend_id)
+
+
+def get_awareness_changes(awareness: Awareness, update: bytes) -> Dict[str, Any]:
+    decoder = Decoder(update)
+    timestamp = int(time.time() * 1000)
+    added = []
+    updated = []
+    filtered_updated = []
+    removed = []
+    length = decoder.read_var_uint()
+    for i in range(length):
+        client_id = decoder.read_var_uint()
+        clock = decoder.read_var_uint()
+        state_str = decoder.read_var_string()
+        state = None if not state_str else json.loads(state_str)
+        client_meta = awareness.meta.get(client_id)
+        prev_state = awareness.states.get(client_id)
+        curr_clock = 0 if client_meta is None else client_meta["clock"]
+        if curr_clock < clock or (curr_clock == clock and state is None and client_id in awareness.states):
+            if state is None:
+                if client_id == awareness.client_id and awareness.get_local_state() is not None:
+                    clock += 1
+                else:
+                    del awareness.states[client_id]
+            else:
+                awareness.states[client_id] = state
+            awareness.meta[client_id] = {
+                "clock": clock,
+                "last_updated": timestamp,
+            }
+            if client_meta is None and state is not None:
+                added.append(client_id)
+            elif client_meta is not None and state is None:
+                removed.append(client_id)
+            elif state is not None:
+                if state != prev_state:
+                    filtered_updated.append(client_id)
+                updated.append(client_id)
+    res = {"added": added, "updated": updated, "filtered_updated": filtered_updated, "removed": removed}
+    return res
